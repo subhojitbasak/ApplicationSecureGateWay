38d7cc2a34b105636045a3fd241824be426cd8de
Subhojit Basak <subhojitbasak840@gmail.com>
2024-07-01 16:05:06 +0530

    Email Notification Integration

--- src/main/java/com/form/login/demo/Service/UserService.java
+++ src/main/java/com/form/login/demo/Service/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,7 +22,8 @@ public class UserService {
     private RestTemplate restTemplate;
 
     public ResponseEntity<String> newUserSignUp(UserInfo userInfo) {
-        UserInfo byEmail = repository.findByEmail(userInfo.getEmail());
+        Optional<UserInfo> byEmail = repository.findByEmail(userInfo.getEmail());
+        Optional<UserInfo> byUsername = repository.findByUsername(userInfo.getUsername());
         String url = "http://localhost:8081/sendfromgateway";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -29,19 +31,26 @@ public class UserService {
         map.put("username", userInfo.getUsername());
         map.put("email", userInfo.getEmail());
         map.put("roles", userInfo.getRoles());
-        if (byEmail == null) {
+        if (byEmail.isEmpty() && byUsername.isEmpty()) {
             try {
                 userInfo.setPassword(encoder.encode(userInfo.getPassword()));
                 repository.save(userInfo);
                 HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
                 restTemplate.postForObject(url, request, Void.class);
-                return ResponseEntity.ok().body("User sucessfully signed up!!");
+                return ResponseEntity.ok().body("User successfully signed up!!");
             } catch (Exception e) {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during registration.\nPlease try again later");
             }
         } else {
 //            System.out.println(byEmail.getUsername());
-            return ResponseEntity.badRequest().body("User is already registered. Chose another email address!!");
+            if(byUsername.isPresent()){
+                return ResponseEntity.badRequest().body("User mane is not unique. Chose another Username!!");
+            }else if(byEmail.isPresent()){
+                return ResponseEntity.badRequest().body("User is already registered. Chose another email address!!");
+
+            }
+            return ResponseEntity.badRequest().body("User is already registered. Chose another email address & Username!!");
+
         }
     }
 
