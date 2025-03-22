32b3a9492f271fdabe5135ee18e33ccbfd4dc1ff
subhojit basak <subhojitbasak840@gmail.com>
2025-03-22 19:49:49 +0530

    configuration update for jasypt fix and release on github

--- src/main/java/com/form/login/demo/ApplicationSecureGateway.java
+++ src/main/java/com/form/login/demo/ApplicationSecureGateway.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableEncryptableProperties
 @SpringBootApplication
 public class ApplicationSecureGateway {
-
-
-
-	public static void main(String[] args) {
-
-		SpringApplication.run(ApplicationSecureGateway.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSecureGateway.class, args);
+    }
 }
